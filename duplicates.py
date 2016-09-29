64a8526acbc645bbfacdd8a1058245b9a687f490
aleksei-g <lexa_prorock@mail.ru>
2016-09-29 16:43:58 +0300

    Реорганизация кода

--- duplicates.py
+++ duplicates.py
@@ -2,29 +2,38 @@ import os
 import argparse
 
 
-def validate_enter_dir(dir1, dir2):
-    list_valid_dir = []
-    for num, valid_dir in enumerate((dir1, dir2), start=1):
-        if os.path.exists(valid_dir):
-            if os.path.isdir(valid_dir):
-                list_valid_dir.append(os.path.abspath(valid_dir))
-            else:
-                print('Enter path %s is not directory!' % num)
+def validate_dir(dir1, dir2):
+    validate_dir_ok = True
+    for validation_dir in dir1, dir2:
+        if not os.path.exists(validation_dir):
+            print('Directory \"%s\" not found!' % validation_dir)
+            validate_dir_ok = False
         else:
-            print('Directory %s not found!' % num)
-    return list_valid_dir
+            if not os.path.isdir(validation_dir):
+                print('Enter path \"%s\" is not directory!' % validation_dir)
+                validate_dir_ok = False
+    return validate_dir_ok
 
 
-def get_full_file_name(file_path):
-    for root, dirs, file_names in os.walk(file_path):
+def get_list_file_names(directory):
+    for root, dirs, file_names in os.walk(directory):
         for file_name in file_names:
             yield os.path.join(root, file_name)
 
 
-def compare_files(file_name_1, file_name_2, duplicate_files_dict):
+def compare_files(file_name_1, file_name_2):
     if file_name_1 != file_name_2:
         if os.path.basename(file_name_1) == os.path.basename(file_name_2):
             if os.path.getsize(file_name_1) == os.path.getsize(file_name_2):
+                return True
+    return False
+
+
+def search_duplicate_files(dir1, dir2):
+    duplicate_files_dict = {}
+    for file_name_1 in get_list_file_names(dir1):
+        for file_name_2 in get_list_file_names(dir2):
+            if compare_files(file_name_1, file_name_2):
                 file_name = os.path.basename(file_name_1)
                 if file_name in duplicate_files_dict:
                     duplicate_files_dict[file_name].extend([file_name_1,
@@ -32,23 +41,16 @@ def compare_files(file_name_1, file_name_2, duplicate_files_dict):
                 else:
                     duplicate_files_dict[file_name] = [file_name_1,
                                                        file_name_2]
-
-
-def are_files_duplicates(file_path_1, file_path_2):
-    duplicate_files_dict = {}
-    for file_name_1 in get_full_file_name(file_path_1):
-        for file_name_2 in get_full_file_name(file_path_2):
-            compare_files(file_name_1, file_name_2, duplicate_files_dict)
     return duplicate_files_dict
 
 
-def print_compare_files(duplicate_files_dict):
-    if duplicate_files_dict:
+def print_duplicate_files(duplicate_files):
+    if duplicate_files:
         print('Found duplicate files!')
-        for num_file, file_name in enumerate(duplicate_files_dict.keys(),
+        for num_file, file_name in enumerate(duplicate_files.keys(),
                                              start=1):
             print('%s. %s:' % (num_file, file_name))
-            file_paths = set(duplicate_files_dict[file_name])
+            file_paths = set(duplicate_files[file_name])
             for num_path, file_path in enumerate(file_paths, start=1):
                 print('\t %s) %s' % (num_path, file_path))
     else:
@@ -68,8 +70,7 @@ def createParser():
 if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args()
-    list_valid_dir = validate_enter_dir(namespace.dir1, namespace.dir2)
-    if len(list_valid_dir) == 2:
-        duplicate_files_dict = are_files_duplicates(list_valid_dir[0],
-                                                    list_valid_dir[1])
-        print_compare_files(duplicate_files_dict)
+    if validate_dir(namespace.dir1, namespace.dir2):
+        print_duplicate_files(
+            search_duplicate_files(os.path.abspath(namespace.dir1),
+                                   os.path.abspath(namespace.dir2)))
